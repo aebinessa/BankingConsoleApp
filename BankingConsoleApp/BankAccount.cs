f826fc9f63142c8aa166b676fd361bee72b05e34
binje <binje@BINJESUS>
2024-04-15 14:37:28 +0300

    Add project files.

---
+++ BankingConsoleApp/BankAccount.cs
@@ -0,0 +1,71 @@
+﻿
+namespace BankingConsoleApp
+{
+    public class BankAccount
+    {
+        private double balance;
+        private double[] transactionAmounts = new double[100];
+        private string[] transactionTypes = new string[100];
+        private int transactionCount = 0;
+
+        public BankAccount(double initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                RecordTransaction(amount, "Deposit");
+                Console.WriteLine($"Deposit of {amount} successful. New balance: {balance}");
+            }
+            else
+            {
+                Console.WriteLine("Deposit amount must be greater than zero.");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0)
+            {
+                if (amount <= balance)
+                {
+                    balance -= amount;
+                    RecordTransaction(amount, "Withdrawal");
+                    Console.WriteLine($"Withdrawal of {amount} successful. New balance: {balance}");
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient funds.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Withdrawal amount must be greater than zero.");
+            }
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public void RecordTransaction(double amount, string type)
+        {
+            transactionAmounts[transactionCount] = amount;
+            transactionTypes[transactionCount] = type;
+            transactionCount++;
+        }
+        public void ShowTransactionHistory()
+        {
+            Console.WriteLine("Transaction History:");
+            for (int i = 0; i < transactionCount; i++)
+            {
+                Console.WriteLine($"{transactionTypes[i]}: {transactionAmounts[i]}");
+            }
+        }
+    }
+}
---
+++ BankingConsoleApp/Program.cs
@@ -0,0 +1,65 @@
+﻿
+
+using BankingConsoleApp;
+using System.Security.Principal;
+
+Console.WriteLine("Welcome to the banco! please enter your name");
+string name = Console.ReadLine();
+Console.WriteLine("Enter your initial account number");
+int accountNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter your initial amount");
+int initialAmount = int.Parse(Console.ReadLine());
+
+BankAccount bankAccount = new BankAccount(initialAmount);
+
+
+while (true)
+{
+    Console.WriteLine("Please choose one of the options below");
+    Console.WriteLine("Main Menu:\r\n1. Deposit \r\n2. Withdraw \r\n3. View Balance \r\n4. Exit ");
+    int chosenOption = int.Parse(Console.ReadLine());
+
+
+    if (chosenOption == 1)
+    {
+        Console.WriteLine("Enter your desired deposit amount");
+        int depositAmount = int.Parse(Console.ReadLine());
+        bankAccount.Deposit(depositAmount);
+    }
+    else if (chosenOption == 2)
+    {
+        Console.WriteLine("Enter your desired withdrawal amount");
+        int withdrawAmount = int.Parse(Console.ReadLine());
+        if (withdrawAmount <= initialAmount)
+        {
+            bankAccount.Withdraw(withdrawAmount);
+        }
+        else
+        {
+            Console.WriteLine("Insufficient funds.");
+        }
+    }
+    else if (chosenOption == 3)
+    {
+        Console.WriteLine($"Your balance is {bankAccount.GetBalance()}");
+    }
+    else if (chosenOption == 4)
+    {
+        Console.WriteLine("bye");
+        bankAccount.ShowTransactionHistory();
+
+
+
+
+        break;
+    }
+    }
+   
+
+
+
+
+
+
+
+
